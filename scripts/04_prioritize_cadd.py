710ca3f1f0ceb4586d124a8e4a415496bc3d362c
Saba Babaei <saba.babaei1@gmail.com>
2026-09-05 23:42:18 +0330

    py scripts/04_prioritize_cadd.py

--- scripts/04_prioritize_cadd.py
+++ scripts/04_prioritize_cadd.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv("data/variants.csv")
+df = pd.read_csv("results/rare_variants.csv")
 
 high_cadd = df[df["CADD"] >= 20]
+
+high_cadd.to_csv("results/high_cadd_variants.csv", index=False)
 
 print(high_cadd)
