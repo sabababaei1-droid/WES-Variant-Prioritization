c924131f312b910e1427a64d88ab909ed17a0634
Saba Babaei <saba.babaei1@gmail.com>
2026-09-05 23:33:34 +0330

    Save frequency filtering results

--- scripts/03_filter_frequency.py
+++ scripts/03_filter_frequency.py
@@ -1,7 +1,9 @@
 import pandas as pd
 
-df = pd.read_csv("data/variants.csv")
+df = pd.read_csv("results/functional_variants.csv")
 
 rare = df[df["gnomAD_AF"] <= 0.01]
+
+rare.to_csv("results/rare_variants.csv", index=False)
 
 print(rare)
