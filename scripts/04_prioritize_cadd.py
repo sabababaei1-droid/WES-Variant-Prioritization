f1189a32864d6a3ac25744ecf55df547bd19a55f
Saba Babaei <saba.babaei1@gmail.com>
2026-09-07 16:00:40 +0330

    Update 04_prioritize_cadd.py

--- scripts/04_prioritize_cadd.py
+++ scripts/04_prioritize_cadd.py
@@ -4,6 +4,6 @@ df = pd.read_csv("results/rare_variants.csv")
 
 high_cadd = df[df["CADD"] >= 20]
 
-high_cadd.to_csv("results/high_cadd_variants.csv", index=False)
+high_cadd.to_csv("results/03_high_cadd_variants.csv", index=False)
 
 print(high_cadd)
