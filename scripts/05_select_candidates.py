b364696c32c0db9c753b8d5affcd496b0c13a41c
Saba Babaei <saba.babaei1@gmail.com>
2026-09-07 15:10:08 +0330

    add 05_select_candidates

--- scripts/05_select_candidates.py
+++ scripts/05_select_candidates.py
@@ -3,11 +3,7 @@ import pandas as pd
 df = pd.read_csv("results/high_cadd_variants.csv")
 
 candidates = df[
-    (df["Consequence"].isin([
-        "missense",
-        "frameshift",
-        "stop_gained"
-    ]))
+    df["ClinVar_Significance"] == "Pathogenic"
 ]
 
 candidates.to_csv("results/candidate_variants.csv", index=False)
